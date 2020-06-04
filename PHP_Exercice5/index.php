dd63fc7a80f2e5f9cbf12e0b323efc0efaf85cb8
unknown <jules.gioux@gmail.com>
2020-06-04 14:09:44 +0200

    Exercice 5

---
+++ PHP_Exercice5/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 5</title>
+</head>
+<body>
+    <?php 
+        settype ($myInt, "integer");
+        echo "$myInt<br>";
+        $myInt = 39;
+        echo $myInt;
+    ?>
+</body>
+</html>
