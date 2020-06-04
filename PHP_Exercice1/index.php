aa510b67bda5eb7f02bf6e2c5531296a7ea90db6
unknown <jules.gioux@gmail.com>
2020-06-04 11:45:39 +0200

    Exercice 2

---
+++ PHP_Exercice1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 1</title>
+</head>
+<body>
+    <?php 
+    $name = 'Julien';
+    print $name;
+    ?>
+</body>
+</html>
---
+++ PHP_Exercice2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 2</title>
+</head>
+<body>
+    <?php 
+    $firstName = 'Julien';
+    $lastName = 'Gioux';
+    $age = 39;
+    printf ($firstName.' '.$lastName.' '.$age.' ans.');
+    ?>
+</body>
+</html>
