7061a085186d73c3096cb225d8e3256d2c7e3d78
unknown <jules.gioux@gmail.com>
2020-06-04 11:58:23 +0200

    Exercice 3

---
+++ PHP_Exercice3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 3</title>
+</head>
+<body>
+    <?php 
+    $km = 1;
+    print ($km.'<br>');
+    $km += 2;
+    print ($km.'<br>');
+    $km += 122;
+    print ($km.'<br>');
+    ?>
+</body>
+</html>
