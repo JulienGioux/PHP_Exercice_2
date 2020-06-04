98dd9e056ace8fc04fe73d06b95b0adcfac25fcf
unknown <jules.gioux@gmail.com>
2020-06-04 12:17:13 +0200

    Exercice 4

---
+++ PHP_Exercice4/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 4</title>
+</head>
+<body>
+    <?php 
+        $tString = "String";
+        $tInt = 39;
+        $tFloat = 3.33333;
+        $tBool = TRUE;
+
+        echo "$tString <br> $tInt <br> $tFloat <br> $tBool";
+    ?>
+</body>
+</html>
